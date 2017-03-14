4b1283f33286c9b2c064986839daa06cad566803
S <s>
2017-03-14 13:41:27 -0500

    {"types":"A","runs":0}

---
+++ FlipHorizontal.java
@@ -0,0 +1,13 @@
+import acm.graphics.*;
+import acm.program.*;
+
+public class FlipHorizontal extends GraphicsProgram {
+	public void run () {
+		GImage originalImage = new GImage("milkmaid.jpg");
+		GImage flippedImage = flipHorizontal(originalImage);
+	}
+	
+	private GImage flipHorizontal(GImage image) {
+		
+	}
+}
