ef1ffcafe5be34a5a0d0f8939197df2bb0f62539
S <s>
2017-03-14 14:04:31 -0500

    {"types":"C","runs":0}

--- FlipHorizontal.java
+++ FlipHorizontal.java
@@ -11,9 +11,13 @@ public class FlipHorizontal extends GraphicsProgram {
 		int height = array.length;
 		int width = array[0].length;
 		for (int i = 0; i < height; i++) {
-			for (int p1 = 0; p1 < width; p1++) {
-				
+			for (int p1 = 0; p1 < width / 2; p1++) {
+				int p2 = width - p1 - 1;
+				int temp = array[i][p1];
+				array[i][p1] = array[i][p2];
+				array[i][p2] = temp;
 			}
 		}
+		return new GImage(array);
 	}
 }
