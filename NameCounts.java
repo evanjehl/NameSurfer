8299c981710a22db26cb27e8ea773afe49dcb5fe
S <s>
2017-03-16 13:45:50 -0500

    {"types":"C","runs":0}

--- NameCounts.java
+++ NameCounts.java
@@ -5,6 +5,14 @@ public class NameCounts extends ConsoleProgram {
 	
 	public void run() {
 		HashMap<String, Integer> names = new HashMap<String, Integer>();
-		
+		while (true) {
+			String name = readLine("Enter name: ");
+			if (name == "") break;
+			if (names.containsKey(name) == false) {
+				names.put(name, 1);
+			} else {
+				names.put(name, names.get(name) + 1);
+			}
+		}
 	}
 }
