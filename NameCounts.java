538c105dedb7e775e557b2299488b8dba3e4ca52
S <s>
2017-03-16 13:39:34 -0500

    {"types":"A","runs":0}

---
+++ NameCounts.java
@@ -0,0 +1,10 @@
+import acm.program.*;
+import java.util.*;
+
+public class NameCounts extends ConsoleProgram {
+	
+	public void run() {
+		HashMap<String, Integer> names = new HashMap<String, Integer>();
+		
+	}
+}
