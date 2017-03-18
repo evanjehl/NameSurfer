a7311037be0642b4f5e32dd93ca267547870c6d0
S <s>
2017-03-18 12:05:06 -0500

    {"types":"C","runs":15}

--- Boxes.java
+++ Boxes.java
@@ -26,7 +26,7 @@ public class Boxes extends GraphicsProgram {
 			GCompound box = new GCompound();
 			String input = nameField.getText();
 			GLabel text = new GLabel(input);
-			text.setLocation((getWidth() - text.getWidth()) / 2, (getHeight() + text.getAscent()) / 2);
+			text.setLocation((getWidth() - text.getWidth()) / 2, (getHeight() + text.getAscent() + text.getDescent()) / 2);
 			add(new GRect(initX, initY, BOX_HEIGHT, BOX_WIDTH));
 			add(text);
 			boxes.put(input, box);
