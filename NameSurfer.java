b91635f42f9e2a6d68295eb33811b614cec9dd69
S <s>
2017-03-20 01:29:50 -0500

    {"types":"C","runs":0}

--- NameSurfer.java
+++ NameSurfer.java
@@ -51,7 +51,7 @@ public class NameSurfer extends Program implements NameSurferConstants {
 	    		String line = rd.readLine();
 	    		if (line == null) break;
 	    		NameSurferEntry entry = new NameSurferEntry(line);
-	    		nameDatabase.put(key, entry);
+	    		nameDatabase.put(entry.getName(), entry);
 	    	} catch (IOException ex) {
 	    		throw new ErrorException(ex);
 	    	}
