d3c8fd957bdd06f9abd7aba71b1a61c829998749
S <s>
2017-03-21 16:45:35 -0500

    {"types":"C","runs":1}

--- NameSurfer.java
+++ NameSurfer.java
@@ -58,7 +58,7 @@ public class NameSurfer extends ConsoleProgram implements NameSurferConstants {
 	    			if (i < NDECADES - 1) {
 	    				tokenEnd = (line.indexOf(" ", tokenEnd));
 	    			} else {
-	    				tokenEnd = line.length();
+	    				tokenEnd = line.length() - 1;
 	    			}
 	    			rankings[i] = Integer.parseInt(line.substring(tokenStart, tokenEnd));
 	    		}
