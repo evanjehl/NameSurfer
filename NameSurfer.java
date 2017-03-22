c14da02fc53d0d0f8c4061fdcdd16f740af23595
S <s>
2017-03-21 21:35:47 -0500

    {"types":"C","runs":8}

--- NameSurfer.java
+++ NameSurfer.java
@@ -63,7 +63,7 @@ public class NameSurfer extends ConsoleProgram implements NameSurferConstants {
 	    			rankings[i] = Integer.parseInt(line.substring(tokenStart, tokenEnd));
 	    		}
 	    		NameSurferEntry entry = new NameSurferEntry(name, rankings);
-	    		println(entry.getName());
+	    		nameDatabase.put(name, entry);
 	    	} catch (IOException ex) {
 	    		throw new ErrorException(ex);
 	    	}
