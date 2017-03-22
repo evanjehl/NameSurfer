72d3ee9cdbf0970e3e3b6c4c2664653eb4121cdb
S <s>
2017-03-22 00:16:37 -0500

    {"types":"C","runs":26}

--- NameSurferGraph.java
+++ NameSurferGraph.java
@@ -77,7 +77,8 @@ public class NameSurferGraph extends GCanvas
 					p1 = new GPoint(getWidth() * j / 11, getHeight() - GRAPH_MARGIN_SIZE);
 					l1 = new GLabel(graphEntries.get(i).getName() + " *", p1.getX() + 2, p1.getY() + 2);
 				} else {
-					p1 = new GPoint(getWidth() * j / 11, ((getHeight() - GRAPH_MARGIN_SIZE) * graphEntries.get(i).getRank(j + 1) / MAX_RANK) + GRAPH_MARGIN_SIZE); 
+					p1 = new GPoint(getWidth() * j / 11, ((getHeight() - GRAPH_MARGIN_SIZE) * graphEntries.get(i).getRank(j + 1) / MAX_RANK) + GRAPH_MARGIN_SIZE);
+					l1 = new GLabel(graphEntries.get(i).getName() + " *", p1.getX() + 2, p1.getY() + 2);
 				}
 				if (graphEntries.get(i).getRank(j + 1) == 0) {
 					p2 = new GPoint(getWidth() * (j + 1) / 11, getHeight() - GRAPH_MARGIN_SIZE);
