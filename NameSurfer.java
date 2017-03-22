4e0c0cd411375d9ebf1f769df395da0881739edf
S <s>
2017-03-22 00:44:12 -0500

    {"types":"C","runs":39}

--- NameSurfer.java
+++ NameSurfer.java
@@ -43,6 +43,7 @@ public class NameSurfer extends Program implements NameSurferConstants {
  */
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == graphButton) {
+			println("This button works.");
 			if (nameDatabase.containsKey(nameField.getText())) {
 				graph.addEntry(nameDatabase.get(nameField.getText()));
 			}
