117786ce55bb53bc02f0ee628fb0ac51faeaceb1
S <s>
2017-03-21 22:57:43 -0500

    {"types":"C","runs":14}

--- NameSurferGraph.java
+++ NameSurferGraph.java
@@ -49,6 +49,7 @@ public class NameSurferGraph extends GCanvas
 	* the size of the canvas changes.
 	*/
 	public void update() {
+		removeAll();
 		for (int i = 0; i < NDECADES; i++) {
 			GLine line = new GLine((getWidth() * i) / 10, 0, (getWidth() * i) / 10, getHeight());
 			GLabel decade = new GLabel("19" + 10 * i, (getWidth() * i) / 10 + 2, getHeight() + 2);
